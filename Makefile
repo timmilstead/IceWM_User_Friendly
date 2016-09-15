384c5662294e7027a62ab45b6edcd3ae08e4e9e1
timmilstead <tim@milstead.me.uk>
2016-09-15 14:46:22 +1200

    Commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+install:
+	cp config/menu /etc/X11/icewm
+	cp config/preferences /etc/X11/icewm
+	
---
+++ install_ubuntu_packages.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+apt-get install icewm gksu gimp scite thunderbird firefox pcmanfm xscreensaver gnome-font-viewer
